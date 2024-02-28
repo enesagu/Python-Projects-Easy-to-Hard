51a08d98593a266d62e427f257a7c9bc81699279
enesagu <97469455+enesagu@users.noreply.github.com>
2024-02-28 17:48:37 +0300

    bug edit

--- PIG/project2.py
+++ PIG/project2.py
@@ -1,5 +1,3 @@
-
-
 with open("story.txt", "r") as f:
     story = f.read()
 
@@ -9,11 +7,10 @@ start_of_word = -1
 target_start = "<"
 target_end = ">"
 
-
 for i, char in enumerate(story):
     if char == target_start:
         start_of_word = i
-    if char ==target_end and start_of_word != -1:
+    if char == target_end and start_of_word != -1:
         word = story[start_of_word: i+1]
         words.add(word)
         start_of_word = -1
@@ -21,7 +18,7 @@ for i, char in enumerate(story):
 answers = {}
 
 for word in words:
-    answers = input("Enter a word for "+ word + ": ")
-    answers[word] = answers
+    answer = input("Enter a word for " + word + ": ")
+    answers[word] = answer
 
 print(answers)
