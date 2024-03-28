e211eb5cacc16cc26aaf29484703fbea987f86a2
Enes Agu <97469455+enesagu@users.noreply.github.com>
2024-03-28 18:40:46 +0300

    use some pandas module

--- 5- Loading_data_with_pandas/loading_pandas.py
+++ 5- Loading_data_with_pandas/loading_pandas.py
@@ -1,1 +1,23 @@
-import pandas
+import pandas as pd
+
+# Read the CSV file
+csv_path = 'basic.csv'
+df = pd.read_csv(csv_path)
+
+# Display the first few rows of the DataFrame
+print(df.head())
+
+# Creating a new DataFrame
+songs = {"Album": ["Ligts Out", "Kazıdık Tırnaklarla", "Ahde vefa", "kan"],
+         "Released": [2019, 2017, 2018, 2020],
+         "Length": ["00:42:19", "00:42:11", "00:57:44", "00:46:21"]}
+songs_frame = pd.DataFrame(songs)
+
+# Filtering rows where the "Released" column has a value greater than or equal to 2018
+df1 = df[df["YEAR"] >= 2020]
+
+# Display the filtered DataFrame
+print(df1)
+
+# Save the filtered DataFrame to a new CSV file
+df1.to_csv("new_songs.csv", index=False)
