18e0ee51c868b71c004f2d890c571d3882ca6d83
Enes Agu <97469455+enesagu@users.noreply.github.com>
2024-04-03 11:47:24 +0300

    create numpy2.py

---
+++ 6-_Numpy_usage/numpy2.py
@@ -0,0 +1,19 @@
+#%%
+import numpy as np
+
+
+a = [[11,12,13],[21,22,23],[31,32,33]]
+
+A = np.array(a)
+
+#%%
+
+
+print("a: ")
+print(a)
+print("A: ")
+print(A)
+print("A.shape: ")
+print(A.shape)
+
+# %%
