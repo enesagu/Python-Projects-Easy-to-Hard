4603e9da792793139d162253242e798f76b0cc4e
Enes Agu <97469455+enesagu@users.noreply.github.com>
2024-04-04 16:55:27 +0300

    update basic_request.py
    I just update file, now we can see more option with request

--- 7- request_HTTP/basic_request.py
+++ 7- request_HTTP/basic_request.py
@@ -5,4 +5,36 @@ url = 'https://www.ibm.com/'
 r = requests.get(url)
 
 print(r) # our response 
-print(r.status_code) # our response code : 200
+print(r.status_code) # our response code : 200 : OK
+
+print(r.headers) # we can get header 
+
+print(r.request.body) # None
+
+
+# we can see some detail in this request
+
+header = r.headers
+
+print(header['date']) # Thu, 04 Apr 2024 13:43:27 GMT
+
+print(header['Content-Type']) # text/html;charset=utf-8
+
+print(r.encoding) # utf-8
+
+
+
+print(r.text[0:])
+'''
+result of r.text[0:100]:
+
+<!DOCTYPE HTML>
+<html lang="en-us">
+<head>
+    
+    
+    
+    
+    <meta charset="UTF-8"/>
+  
+''' # we can show more option with r.text[0:] or just r.texts 
