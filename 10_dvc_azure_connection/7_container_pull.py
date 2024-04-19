b4563724e7edc701799927ba8757963f012a3329
Enes Agu <97469455+enesagu@users.noreply.github.com>
2024-04-19 14:03:53 +0300

    update container_pull

--- 10_dvc_azure_connection/7_container_pull.py
+++ 10_dvc_azure_connection/7_container_pull.py
@@ -1,40 +1,40 @@
 from azure.storage.blob import BlobServiceClient
 import os
 
-def download_blob(storage_connection_string, container_name, local_folder):
-    # Create Blob service client
-    blob_service_client = BlobServiceClient.from_connection_string(storage_connection_string)
+def download_container(connection_string, container_name, local_folder):
+    # Connection to Blob Service
+    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
 
-    # Get container client
+    # Get Container Client
     container_client = blob_service_client.get_container_client(container_name)
 
     # List blobs in the container
     blob_list = container_client.list_blobs()
 
-    # Ensure local folder exists
+    # Create local folder if it doesn't exist
     os.makedirs(local_folder, exist_ok=True)
 
     # Download each blob
     for blob in blob_list:
-        # Construct blob name and destination file path
-        blob_name = blob.name
-        download_file_path = os.path.join(local_folder, blob_name)
-
-        # Download the blob
+        blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob.name)
+        download_file_path = os.path.join(local_folder, blob.name)
         with open(download_file_path, "wb") as download_file:
-            download_file.write(container_client.download_blob(blob_name).readall())
+            download_file.write(blob_client.download_blob().readall())
 
-    print("All blobs have been successfully downloaded.")
+def main():
+    # Azure account info
+    connection_string = "string"
+
+    # Container name
+    container_name = "name"
+
+    # Local folder to save the downloaded files
+    local_folder = "path"
+
+    # Download container
+    download_container(connection_string, container_name, local_folder)
+
+    print("Container downloaded successfully.")
 
 if __name__ == "__main__":
-    # Azure Blob storage connection string
-    storage_connection_string = "string"
-    
-    # Name of the Blob container to download from
-    container_name = "name"
-    
-    # Local folder where downloaded files will be saved
-    local_folder = "test"
-
-    # Download blobs
-    download_blob(storage_connection_string, container_name, local_folder)
+    main()
