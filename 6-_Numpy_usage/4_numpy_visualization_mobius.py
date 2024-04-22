047bef0f2e6630001f8654027af83e0b6ce7cc23
Enes Agu <97469455+enesagu@users.noreply.github.com>
2024-04-22 13:16:11 +0300

    numpy "visualization" added. and modified.

---
+++ 6-_Numpy_usage/4_numpy_visualization_mobius.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+# Mobius  paramethers
+theta = np.linspace(0, 2 * np.pi, 100)
+w = np.linspace(-0.25, 0.25, 8)
+theta, w = np.meshgrid(theta, w)
+phi = 0.5 * w
+
+# Mobius  x, y ve z
+x = (1 + w * np.cos(theta / 2)) * np.cos(phi)
+y = (1 + w * np.cos(theta / 2)) * np.sin(phi)
+z = w * np.sin(theta / 2)
+
+# 3B visulation
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot_surface(x, y, z, cmap='viridis')
+
+# Eksenlerin ayarlanması
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+ax.set_title('Mobius Şeridi')
+
+# Görselleştirmenin gösterilmesi
+plt.show()
